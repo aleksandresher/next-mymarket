ebcaa2e4f45e9512556d6527c443d26c0b256ba3
Aleksandre Shervashidze <aleksandre.shervashidze.2@iliauni.edu.ge>
2024-04-10 19:52:18 +0400

    vip and vip+ components changed to server component

--- src/components/vipes/vip/Vip.tsx
+++ src/components/vipes/vip/Vip.tsx
@@ -1,38 +1,20 @@
-"use client";
-import Image from "next/image";
 import CarouselWrapper from "../../data-creators/carousel/Carousel";
-import { useQuery } from "@tanstack/react-query";
 import { loadVipProducts } from "@lib/load-vip";
-import { LaptopType, MobileType } from "@lib/mobile-laptop";
 import { Suspense } from "react";
 import CarouselSkeleton from "@components/skeletons/CarouselSkeleton";
 
-export default function Vip() {
+export default async function Vip() {
   const title = "VIP";
   const image = "/vip/vip.svg";
-  const { data, isLoading, error } = useQuery<Array<MobileType | LaptopType>>({
-    queryKey: ["vipproducts"],
-    queryFn: () => loadVipProducts(),
-  });
 
-  if (isLoading) {
-    return (
-      <section className="w-full flex justify-center px-12">
-        <div className="w-11/12 flex flex-col gap-6 mt-8 items-center mb-9">
-          <CarouselSkeleton image={image} title={title} />
-        </div>
-      </section>
-    );
-  }
-
-  if (error) {
-    return <div>Error</div>;
-  }
+  const data = await loadVipProducts();
 
   return (
     <section className="w-full flex justify-center px-12">
       <div className="w-11/12 flex flex-col gap-6 mt-8 items-center mb-9">
-        <CarouselWrapper data={data || []} title={title} image={image} />
+        <Suspense fallback={<CarouselSkeleton title={title} image={image} />}>
+          <CarouselWrapper data={data || []} title={title} image={image} />
+        </Suspense>
       </div>
     </section>
   );
--- src/components/vipes/vipplus/VipPlus.tsx
+++ src/components/vipes/vipplus/VipPlus.tsx
@@ -1,38 +1,20 @@
-"use client";
-import Image from "next/image";
 import CarouselWrapper from "../../data-creators/carousel/Carousel";
-import { useQuery } from "@tanstack/react-query";
 import { loadVipplusProducts } from "@lib/load-vipplus";
-import { LaptopType, MobileType } from "@lib/mobile-laptop";
 import { Suspense } from "react";
 import CarouselSkeleton from "@components/skeletons/CarouselSkeleton";
 
-export default function VipPlus() {
+export default async function VipPlus() {
   const title = "VIP+";
   const image = "/vip/vipplus.svg";
-  const { data, isLoading, error } = useQuery<Array<MobileType | LaptopType>>({
-    queryKey: ["vipplusproducts"],
-    queryFn: () => loadVipplusProducts(),
-  });
 
-  if (isLoading) {
-    return (
-      <section className="w-full flex justify-center px-12">
-        <div className="w-11/12 flex flex-col gap-6 mt-8 items-center mb-9">
-          <CarouselSkeleton image={image} title={title} />
-        </div>
-      </section>
-    );
-  }
-
-  if (error) {
-    return <div>Error</div>;
-  }
+  const data = await loadVipplusProducts();
 
   return (
     <section className="w-full flex justify-center px-12">
       <div className="w-11/12 flex flex-col gap-6 mt-8 items-center mb-9">
-        <CarouselWrapper data={data || []} title={title} image={image} />
+        <Suspense fallback={<CarouselSkeleton title={title} image={image} />}>
+          <CarouselWrapper data={data || []} title={title} image={image} />
+        </Suspense>
       </div>
     </section>
   );
