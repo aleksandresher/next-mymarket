e12a4ac7832014c0c682819124d7f7ad0caf5d0c
Aleksandre Shervashidze <aleksandre.shervashidze.2@iliauni.edu.ge>
2024-04-10 19:47:39 +0400

    change super vip product component to server component

--- src/components/vipes/supervip/SuperVip.tsx
+++ src/components/vipes/supervip/SuperVip.tsx
@@ -1,37 +1,20 @@
-"use client";
-import Image from "next/image";
 import CarouselWrapper from "../../data-creators/carousel/Carousel";
-import { useQuery } from "@tanstack/react-query";
 import { loadSuperVipProducts } from "@lib/load-supervip";
-import { LaptopType, MobileType } from "@lib/mobile-laptop";
 import { Suspense } from "react";
 import CarouselSkeleton from "@components/skeletons/CarouselSkeleton";
 
-export default function SuperVip() {
+export default async function SuperVip() {
   const title = "SUPER VIP";
   const image = "/vip/supervip.svg";
-  const { data, isLoading, error } = useQuery<Array<MobileType | LaptopType>>({
-    queryKey: ["supervipproducts"],
-    queryFn: () => loadSuperVipProducts(),
-  });
 
-  if (isLoading) {
-    return (
-      <section className="w-full flex justify-center px-12">
-        <div className="w-11/12 flex flex-col gap-6 mt-8 items-center mb-9">
-          <CarouselSkeleton image={image} title={title} />
-        </div>
-      </section>
-    );
-  }
-
-  if (error) {
-    return <div>Error</div>;
-  }
+  const data = await loadSuperVipProducts();
 
   return (
     <section className="w-full flex justify-center px-12">
       <div className="w-11/12 flex flex-col gap-6 mt-8 items-center mb-9">
+        <Suspense
+          fallback={<CarouselSkeleton image={image} title={title} />}
+        ></Suspense>
         <CarouselWrapper data={data || []} title={title} image={image} />
       </div>
     </section>
