0718275da4275b897a005be8be89c953bebc8277
Aleksandre Shervashidze <aleksandre.shervashidze.2@iliauni.edu.ge>
2024-04-14 02:38:44 +0400

    comment auth

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,72 +1,72 @@
-import NextAuth from "next-auth/next";
-import GoogleProvider from "next-auth/providers/google";
-import { connectToDB } from "@/utils/database";
-import CredentialsProvider from "next-auth/providers/credentials";
+// import NextAuth from "next-auth/next";
+// import GoogleProvider from "next-auth/providers/google";
+// import { connectToDB } from "@/utils/database";
+// import CredentialsProvider from "next-auth/providers/credentials";
 
-import User from "../../../../../models/User";
+// import User from "../../../../../models/User";
 
-const handler = NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-    CredentialsProvider({
-      name: "Credentials",
+// const handler = NextAuth({
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_ID,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//     }),
+//     CredentialsProvider({
+//       name: "Credentials",
 
-      credentials: {
-        email: {
-          label: "Email",
-          type: "text",
-          placeholder: "example@gmail.com",
-        },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials, req) {
-        // const user = { id: "1", name: "J smith", email: "sjsd@gmail.com" };
-        const user = await User.findOne({ email: req.email });
+//       credentials: {
+//         email: {
+//           label: "Email",
+//           type: "text",
+//           placeholder: "example@gmail.com",
+//         },
+//         password: { label: "Password", type: "password" },
+//       },
+//       async authorize(credentials, req) {
+//         // const user = { id: "1", name: "J smith", email: "sjsd@gmail.com" };
+//         const user = await User.findOne({ email: req.email });
 
-        if (user) {
-          return user;
-        } else {
-          return null;
-        }
-      },
-    }),
-  ],
+//         if (user) {
+//           return user;
+//         } else {
+//           return null;
+//         }
+//       },
+//     }),
+//   ],
 
-  callbacks: {
-    async session({ session, status }) {
-      const sessionUser = await User.findOne({
-        email: session.user.email,
-      });
+//   callbacks: {
+//     async session({ session, status }) {
+//       const sessionUser = await User.findOne({
+//         email: session.user.email,
+//       });
 
-      session.user.id = sessionUser._id.toString();
-      session.user.role = sessionUser.role;
-      session.user.status = status;
+//       session.user.id = sessionUser._id.toString();
+//       session.user.role = sessionUser.role;
+//       session.user.status = status;
 
-      return session;
-    },
-    async signIn({ profile }) {
-      try {
-        await connectToDB();
+//       return session;
+//     },
+//     async signIn({ profile }) {
+//       try {
+//         await connectToDB();
 
-        const userExists = await User.findOne({ email: profile.email });
+//         const userExists = await User.findOne({ email: profile.email });
 
-        if (!userExists) {
-          await User.create({
-            email: profile.email,
-            username: profile.name.replace(" ", "").toLowerCase(),
-            role: "user",
-          });
-        }
-        return true;
-      } catch (error) {
-        console.log(error);
-        return false;
-      }
-    },
-  },
-});
+//         if (!userExists) {
+//           await User.create({
+//             email: profile.email,
+//             username: profile.name.replace(" ", "").toLowerCase(),
+//             role: "user",
+//           });
+//         }
+//         return true;
+//       } catch (error) {
+//         console.log(error);
+//         return false;
+//       }
+//     },
+//   },
+// });
 
-export { handler as GET, handler as POST };
+// export { handler as GET, handler as POST };
